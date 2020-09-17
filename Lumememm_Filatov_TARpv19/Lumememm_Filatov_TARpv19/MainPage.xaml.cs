bc8b65376f0c53708159334305c511220681471a
opilane <opilane@HP-CZC6297B9X>
2020-09-17 10:52:03 +0300

    Координаты  Снеговика

--- Lumememm_Filatov_TARpv19/Lumememm_Filatov_TARpv19/MainPage.xaml.cs
+++ Lumememm_Filatov_TARpv19/Lumememm_Filatov_TARpv19/MainPage.xaml.cs
@@ -15,19 +15,19 @@ namespace Lumememm_Filatov_TARpv19
             AbsoluteLayout absoluteLayout = new AbsoluteLayout();
 
             BoxView redBox = new BoxView { BackgroundColor = Color.Aqua, CornerRadius=110 };
-            BoxView greenBox = new BoxView { BackgroundColor = Color.Aqua,CornerRadius = 110 };
+            BoxView greenBox = new BoxView { BackgroundColor = Color.Aqua, CornerRadius = 110 };
             BoxView blueBox = new BoxView { BackgroundColor = Color.Aqua, CornerRadius = 110 };
 
-            AbsoluteLayout.SetLayoutBounds(redBox, new Rectangle(0.5, 0.0, 150, 150));
+            AbsoluteLayout.SetLayoutBounds(redBox, new Rectangle(0.5, 0.0, 120, 120));
             // устанавливаем пропорциональные координаты
             AbsoluteLayout.SetLayoutFlags(redBox, AbsoluteLayoutFlags.PositionProportional);
 
-            AbsoluteLayout.SetLayoutBounds(greenBox, new Rectangle(0.5, 0.4, 200, 200));
+            AbsoluteLayout.SetLayoutBounds(greenBox, new Rectangle(0.5, 0.3, 165, 165));
             AbsoluteLayout.SetLayoutFlags(greenBox, AbsoluteLayoutFlags.PositionProportional);
 
-            AbsoluteLayout.SetLayoutBounds(blueBox, new Rectangle(0.8, 0.4, 200, 200));
+            AbsoluteLayout.SetLayoutBounds(blueBox, new Rectangle(0.5, 0.9, 250, 250));
             // пропорциональные координаты и размеры
-            AbsoluteLayout.SetLayoutFlags(blueBox, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutFlags(blueBox, AbsoluteLayoutFlags.PositionProportional);
 
             absoluteLayout.Children.Add(redBox);
             absoluteLayout.Children.Add(greenBox);
